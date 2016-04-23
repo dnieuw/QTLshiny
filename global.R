68034126bd4043a54c7ca2099510a1ee7d0cc191
dnieuw <davidnieuwenhuijse@gmail.com>
2016-04-23 19:10:11 +0200

    Updated formatting of code and added tabs to quality tab

--- global.R
+++ global.R
@@ -5,15 +5,19 @@ library(ggplot2)
 library(qtl)      
 library(ASMap)
 library(shinythemes)
-data(hyper)
+library(ggthemes)
+library(ggiraph)
+library(gtools)
+library(dplyr)
 
 
 
-# mydata <- read.cross(format = "csv",
-#            file = "data/cross.csv" ,
-#            genotypes = c("a","h","b"),
-#            alleles = c("a","b"),
-#            estimate.map = FALSE,
-#            BC.gen = 0,
-#            F.gen = 6
-# )
+mydata <- read.cross(format = "csv",
+           file = "data/cross.csv" ,
+           genotypes = c("a","h","b"),
+           alleles = c("a","b"),
+           estimate.map = FALSE,
+           BC.gen = 0,
+           F.gen = 6
+)
+
--- ui.R
+++ ui.R
@@ -1,44 +1,64 @@
 shinyUI(
   navbarPage("Map construction", inverse = TRUE, theme = shinytheme("cerulean"),
-                   tabPanel("Clean data",
-                            sidebarLayout(
-                              sidebarPanel(
-                                fileInput('file1', 'Choose file to upload',
-                                          accept = c(
-                                            'text/csv',
-                                            'text/comma-separated-values',
-                                            'text/tab-separated-values',
-                                            'text/plain',
-                                            '.csv',
-                                            '.tsv'
-                                          )),
-                                br(),
-                                selectizeInput('e1',
-                                               label = 'Data exploration',
-                                               choices = c("Remove duplicates","Filter missing")
-                                )),
-                              mainPanel(
-                                iplotMap_output('plot')
-                              )
-                   )),
-                   tabPanel("Quality plots",
-                            renderDataTable("summary")
-                   ),
-                   tabPanel("Genetic map",
-                            renderDataTable("summary")
-                   ),
-                   tabPanel("QTL mapping",
-                            sidebarLayout(
-                              sidebarPanel(
-                               selectizeInput('e2','Interval mapping',
-                                               choices = c("Interval mapping","Stepwise")
-                                ),
-                              htmlOutput("selectUI1")),
-                              mainPanel(
-                                # iplotScanone_output('scanone', width = "100%", height = "580")
-                                plotOutput('scanone')
-                                ))),
-                   tabPanel("PDF report",
-                            renderDataTable("summary")
-                   )
-))
+    tabPanel("Clean data",
+      sidebarLayout(
+        sidebarPanel(
+          fileInput('file1', 'Choose file to upload',
+            accept = c(
+              'text/csv',
+              'text/comma-separated-values',
+              'text/tab-separated-values',
+              'text/plain',
+              '.csv',
+              '.tsv'
+            )
+          ),
+          br(),
+          selectizeInput('e1',
+            label = 'Data exploration',
+            choices = c("Remove duplicates","Filter missing")
+          )
+        ),
+        mainPanel(
+          iplotMap_output('plot')
+        )
+      )
+    ),
+    tabPanel("Quality plots",
+  		tabsetPanel(
+	   		tabPanel("Plot",
+	   			sidebarLayout(
+		        sidebarPanel(
+		          uiOutput("GenPosLG.slider"),
+		          verbatimTextOutput("GenPosLG.slider.output")
+		        ),
+		        mainPanel(
+		        )
+		      )
+	   		),
+	    	tabPanel("Summary"),
+	    	tabPanel("Table")
+  		)
+    ),
+    tabPanel("Genetic map",
+      renderDataTable("summary")
+    ),
+    tabPanel("QTL mapping",
+      sidebarLayout(
+        sidebarPanel(
+          selectizeInput('e2','Interval mapping',
+            choices = c("Interval mapping","Stepwise")
+          ),
+          htmlOutput("selectUI1")
+        ),
+        mainPanel(
+          # iplotScanone_output('scanone', width = "100%", height = "580")
+          plotOutput('scanone')
+        )
+      )
+    ),
+    tabPanel("PDF report",
+      renderDataTable("summary")
+    )
+  )
+)
